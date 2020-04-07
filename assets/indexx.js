e3ad86f1da9fb8f01ce406eba40589c73ad0c7bb
EnggarSe <enggarseptrinass@gmail.com>
2020-04-07 18:31:42 +0700

    CRUD-Login

---
+++ assets/indexx.js
@@ -0,0 +1,98 @@
+const buttonAdd = document.getElementById("submitForm")
+const buttonSearch = document.getElementById("searchList")
+const addList = (event)=>{
+    event.preventDefault();
+    let addBarang = document.getElementById("inlineFormInputName2").value;
+    let todos = get();
+    if(addBarang){
+        todos.push(addBarang);
+        save(todos);
+        tampilkanBarang();
+    }
+}
+
+let get = () =>{
+    if(localStorage.todos){
+        return JSON.parse(localStorage.todos);
+    }
+    else{
+        localStorage.todos = "[]";
+        return [];
+    }
+}
+let save = (barang) =>{
+    localStorage.todos = JSON.stringify(barang)
+}
+
+let tampilkanBarang = (barang = get())=>{
+    let tampil = document.getElementById("list-todos");
+    if(barang.length!=0){
+        tampil.innerHTML = "";
+        for(let i = 0 ; i<barang.length ; i++){
+            tampil.innerHTML += `<li class ="">
+            <span id = barang-${i}>${barang[i]}</span>
+            <button id = hapus-${i} class="btn btn-outline-dark btn-sm" aria-hidden="true" aria-hidden="true" onclick = "hapusButton(this)" >hapus</button>
+            <button id = edit-${i} class="btn btn-outline-dark btn-sm" aria-hidden="true"  onclick = "editButton(this)">edit</button> 
+            </li>`
+        }
+    }
+}
+
+let tampilkanSearch = (barang = get())=>{
+    let tampil = document.getElementById("list-todos");
+    if(barang.length!=0){
+        tampil.innerHTML = "";
+        for(let i = 0 ; i<barang.length ; i++){
+            tampil.innerHTML += `<li class ="d-flex justify-content-between">
+            <span id = barang-${i}>${barang[i]}</span> 
+
+            </li> `
+        }
+    }
+}
+const hapusButton = (posisi)=>{
+    let todos = get();
+    let id = posisi.id.replace("hapus-", "");
+    console.log(id);
+    
+    todos.splice(id,1);
+    save(todos);
+    tampilkanBarang();
+}
+
+const editButton = (posisi)=>{
+    let todos = get();
+    let id = posisi.id.replace("edit-", "")
+    const ubah = prompt(`ubah ${todos[id]}`)
+    if(ubah){
+        todos[id] = ubah ;
+        save(todos);
+        tampilkanBarang();
+    }
+    else{
+        alert("Edit Belum di lakukan")
+        tampilkanBarang();
+    }
+    
+}
+const searchList = (event)=>{
+    event.preventDefault();
+    let todos = get();
+    let textSearch = document.getElementById("inlineFormInputName1").value.toLowerCase();
+    let result = todos.filter(item=>{
+        if(item.includes(textSearch)){
+            return item;
+        }
+    })
+    if (result.length!=0){
+        tampilkanSearch(result);
+        
+        
+    }
+    else{
+        alert("Barang Tidak Di Temukan")
+    }
+    
+}
+buttonAdd.addEventListener('click', addList);
+buttonSearch.addEventListener('click', searchList)
---
+++ assets/register.js
@@ -0,0 +1,52 @@
+const registerForm = document.getElementById("registerForm")
+
+
+const userRegistration = (event) => {
+    count = 0;
+    var userDataAll = [];
+    event.preventDefault();
+    var emailInput = document.getElementById("email").value;
+    var namaInput = document.getElementById("nama").value;
+    var passwordInput = document.getElementById("password").value;
+    if(email === "" && nama ==="" && password ===""){
+        alert("form tidak bisa kosong")
+    }
+    else{
+        const userData = {
+            email : emailInput,
+            nama : namaInput, 
+            password: passwordInput
+        };
+        userDataAll = localStorage.getItem('userData') === null ? [] : JSON.parse(localStorage.getItem('userData'));
+        // console.log(userDataAll);
+        let userCek = JSON.parse(localStorage.getItem('userData'));
+        if(userCek == null){
+            userDataAll.push(userData) ;
+            localStorage.setItem("userData", JSON.stringify(userDataAll));
+            alert("Registration Success")
+            window.location.href = `${window.origin}/login.html` ;
+            
+        }
+        else{
+            for(let i = 0 ; i<userCek.length ; i++){
+                if(emailInput===userCek[i].email){
+                    count++
+                    alert("email telah terdaftar");
+                    break;
+                }
+            }
+            if(count==0){
+                userDataAll.push(userData) ;
+                localStorage.setItem("userData", JSON.stringify(userDataAll));
+                alert("Registration Success")
+                window.location.href = `${window.origin}/login.html` ;
+
+            }
+        }
+       
+        
+       
+    }
+}
+
+registerForm.addEventListener("submit", userRegistration)
